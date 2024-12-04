522c326256c37d068cb8c99f9bc243dfdf0086eb
johnkoussertari <121625620+johnkoussertari@users.noreply.github.com>
2024-12-04 14:49:51 +0000

    Updated all files
    Updated all files and index.html

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 
 
 
-let eventList;
+// let eventList;
 
-document.getElementByIdelement("submit") .onclick = function (){
-    eventName = document.getElementById("event").value;
-    console.log(eventList);
-}
+// document.getElementByIdelement("submit") .onclick = function (){
+//     eventName = document.getElementById("event").value;
+//     console.log(eventList);
+// }
 
+
+
+
+
+
+
+// let el = document.getElementById("GFG");
+
+// let value = "GFG_2";
+
+// function getText() {
+//     let a = document.getElementById("eventList");
+//     for (let i = 0; i < a.length; i++) {
+//         let option = a.options[i];
+//         if (option.value == val) {
+//             el.innerHTML = option.text;
+//         }
+//     }
+// }
 
 
